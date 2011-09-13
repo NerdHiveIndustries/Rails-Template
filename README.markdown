23573ec87671d93fc6d7401a9dbfd74deb883998
Joseph McCullough <joseph@vertstudios.com>
2011-09-13 09:30:54 -0500

    Initial Commit

---
+++ README.markdown
@@ -0,0 +1,8 @@
+# Rails Template
+Getting rspec, spork, and autotest to play nicely with rails can be a challenge - especially when working on the same project from linux and OS X. This template comes with the configurations needed to successfully execute rspec autotesting with spork preforking. Just clone this repo as opposed to using 'rails new'
+
+## Instructions
+1. Clone this repo to begin your project
+2. Execute 'rails g rename_to YourApp' to name your app. 
+3. ???
+4. Profit
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '0421fe4c75b96e2a30525dfac6c8be582b78102c56d3ff4976a2beffe7df2012aac4950553affa38b48bcfda64e35759c50097008d30a5954203e508f21bf1fb'
