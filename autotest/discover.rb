ee8e768b45599b2ca34d4ec96947fe23c8d50e1e
Joseph McCullough <joseph@vertstudios.com>
2012-01-08 16:22:58 -0600

    Removed unused autotest folder

--- autotest/discover.rb
+++
@@ -1,8 +0,0 @@
-Autotest.add_discovery { "rails" }
-Autotest.add_discovery { "rspec2" }
-
-Autotest.add_hook(:initialize) do |at|
-  at.add_mapping(%r%^(models|controllers|routing|views|helpers|mailers|requests|lib)/.*rb$%) do |filename, _|
-    filename
-  end
-end
